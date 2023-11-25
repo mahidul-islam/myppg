d9a303cbd6a232dfd0993ff38357cbacc146461f
mahidul-islam <mizihan84@gmail.com>
2023-11-25 22:52:23 +0600

    ui added

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,11 +1,9 @@
 import 'dart:async';
 
 import 'package:camera/camera.dart';
-import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:myppg/util.dart';
 import 'package:wakelock_plus/wakelock_plus.dart';
-// import 'package:universal_html/html.dart' as html;
 
 class SensorValue {
   final DateTime time;
@@ -25,18 +23,15 @@ class HomePage extends StatefulWidget {
 
 class HomePageView extends State<HomePage> with SingleTickerProviderStateMixin {
   bool _toggled = false; // toggle button value
+  bool _breathing = false; // toggle button value
   final List<SensorValue> _data =
       List.empty(growable: true); // array to store the values
   CameraController? _controller;
-  final double _alpha = 0.3; // factor for the mean value
   late AnimationController _animationController;
   double _iconScale = 1;
-  int _bpm = 0; // beats per minute
   final int _fs = 30; // sampling frequency (fps)
   final int _windowLen = 30 * 6; // window length to display - 6 seconds
   CameraImage? _image; // store the last camera image
-  double? _avg; // store the average value during calculation
-  DateTime? _now; // store the now Datetime
   Timer? _timer; // timer for image processing
   Image? imageOutput;
 
@@ -71,100 +66,130 @@ class HomePageView extends State<HomePage> with SingleTickerProviderStateMixin {
         child: Column(
           children: <Widget>[
             Expanded(
-                flex: 2,
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: <Widget>[
-                    Expanded(
-                      flex: 1,
-                      child: Padding(
-                        padding: const EdgeInsets.all(12),
-                        child: ClipRRect(
-                          borderRadius: const BorderRadius.all(
-                            Radius.circular(18),
-                          ),
-                          child: Stack(
-                            fit: StackFit.expand,
-                            alignment: Alignment.center,
-                            children: <Widget>[
-                              _toggled
-                                  ? AspectRatio(
-                                      aspectRatio:
-                                          _controller?.value.aspectRatio ?? 0.0,
-                                      child: CameraPreview(_controller!),
-                                    )
-                                  : Container(
-                                      padding: const EdgeInsets.all(12),
-                                      alignment: Alignment.center,
-                                      color: Colors.grey,
-                                    ),
-                              Container(
-                                alignment: Alignment.center,
-                                padding: const EdgeInsets.all(4),
-                                child: Text(
-                                  _toggled
-                                      ? "Cover both the camera and the flash with your finger"
-                                      : "Camera feed will display here",
-                                  style: TextStyle(
-                                      backgroundColor: _toggled
-                                          ? Colors.white
-                                          : Colors.transparent),
-                                  textAlign: TextAlign.center,
-                                ),
-                              )
-                            ],
-                          ),
+              flex: 2,
+              child: Row(
+                children: [
+                  Expanded(
+                    flex: 1,
+                    child: Padding(
+                      padding: const EdgeInsets.all(12),
+                      child: ClipRRect(
+                        borderRadius: const BorderRadius.all(
+                          Radius.circular(18),
+                        ),
+                        child: Stack(
+                          fit: StackFit.expand,
+                          alignment: Alignment.center,
+                          children: <Widget>[
+                            _toggled
+                                ? AspectRatio(
+                                    aspectRatio:
+                                        _controller?.value.aspectRatio ?? 0.0,
+                                    child: CameraPreview(_controller!),
+                                  )
+                                : Container(
+                                    padding: const EdgeInsets.all(12),
+                                    alignment: Alignment.center,
+                                    color: Colors.grey,
+                                  ),
+                            Container(
+                              alignment: Alignment.center,
+                              padding: const EdgeInsets.all(4),
+                              child: Text(
+                                _toggled
+                                    ? "Cover both the camera and the flash with your finger"
+                                    : "Camera feed will display here",
+                                style: TextStyle(
+                                    backgroundColor: _toggled
+                                        ? Colors.white
+                                        : Colors.transparent),
+                                textAlign: TextAlign.center,
+                              ),
+                            )
+                          ],
                         ),
                       ),
                     ),
-                    Expanded(
-                      flex: 1,
-                      child: Center(
-                          child: Column(
-                        mainAxisSize: MainAxisSize.min,
-                        crossAxisAlignment: CrossAxisAlignment.center,
-                        children: <Widget>[
-                          const Text(
-                            "Estimated BPM",
-                            style: TextStyle(fontSize: 18, color: Colors.grey),
-                          ),
-                          Text(
-                            (_bpm > 30 && _bpm < 150 ? _bpm.toString() : "--"),
-                            style: const TextStyle(
-                                fontSize: 32, fontWeight: FontWeight.bold),
-                          ),
-                        ],
-                      )),
+                  ),
+                  Expanded(
+                    flex: 1,
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        const Text('Created RGB IMAGE'),
+                        SizedBox(child: imageOutput),
+                      ],
                     ),
-                  ],
-                )),
+                  ),
+                ],
+              ),
+            ),
             Expanded(
               flex: 1,
-              child: Center(
-                child: Transform.scale(
-                  scale: _iconScale,
-                  child: IconButton(
-                    icon:
-                        Icon(_toggled ? Icons.favorite : Icons.favorite_border),
-                    color: Colors.red,
-                    iconSize: 128,
-                    onPressed: () {
-                      if (_toggled) {
-                        _untoggle();
-                      } else {
-                        _toggle();
-                      }
-                    },
+              child: Row(
+                children: [
+                  Expanded(
+                    flex: 1,
+                    child: Transform.scale(
+                      scale: _iconScale,
+                      child: IconButton(
+                        icon: Icon(
+                            _toggled ? Icons.favorite : Icons.favorite_border),
+                        color: Colors.red,
+                        iconSize: 128,
+                        onPressed: () {
+                          if (_toggled) {
+                            _untoggle();
+                          } else {
+                            _toggle();
+                          }
+                        },
+                      ),
+                    ),
                   ),
-                ),
+                  Expanded(
+                    flex: 1,
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.spaceAround,
+                      children: [
+                        Text(_breathing ? 'Breathing In' : 'Breathing Out'),
+                        Transform.scale(
+                          scale: _iconScale,
+                          child: IconButton(
+                            icon: const Icon(Icons.air),
+                            color: _breathing ? Colors.red : Colors.black38,
+                            iconSize: 128,
+                            onPressed: () {
+                              setState(() {
+                                _breathing = !_breathing;
+                              });
+                            },
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ],
               ),
             ),
             Expanded(
               flex: 2,
               child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
                 children: [
-                  const Text('Created RGB IMAGE'),
-                  SizedBox(child: imageOutput),
+                  const Text('Data size 20kb'),
+                  const SizedBox(height: 20),
+                  MaterialButton(
+                    color: Colors.amberAccent,
+                    onPressed: () {},
+                    child: const Text('Save on File'),
+                  ),
+                  const SizedBox(height: 20),
+                  MaterialButton(
+                    color: Colors.amberAccent,
+                    onPressed: () {},
+                    child: const Text('Upload to Backend'),
+                  ),
                 ],
               ),
             )
@@ -196,7 +221,6 @@ class HomePageView extends State<HomePage> with SingleTickerProviderStateMixin {
       });
       // after is toggled
       _initTimer();
-      _updateBPM();
     });
   }
 
@@ -236,7 +260,7 @@ class HomePageView extends State<HomePage> with SingleTickerProviderStateMixin {
     _timer = Timer.periodic(Duration(milliseconds: 1000 ~/ _fs), (timer) {
       if (_toggled) {
         if (_image != null) {
-          _scanImage(_image!);
+          // _scanImage(_image!);
         }
       } else {
         timer.cancel();
@@ -244,69 +268,16 @@ class HomePageView extends State<HomePage> with SingleTickerProviderStateMixin {
     });
   }
 
-  void _scanImage(CameraImage image) {
-    _now = DateTime.now();
-    _avg =
-        image.planes.first.bytes.reduce((value, element) => value + element) /
-            image.planes.first.bytes.length;
-    if (_data.length >= _windowLen) {
-      _data.removeAt(0);
-    }
-    setState(() {
-      _data.add(SensorValue(_now ?? DateTime.now(), 255 - (_avg ?? 0.0)));
-    });
-  }
-
-  void _updateBPM() async {
-    // Bear in mind that the method used to calculate the BPM is very rudimentar
-    // feel free to improve it :)
-
-    // Since this function doesn't need to be so "exact" regarding the time it executes,
-    // I only used the a Future.delay to repeat it from time to time.
-    // Ofc you can also use a Timer object to time the callback of this function
-    List<SensorValue> values;
-    double avg;
-    int n;
-    double m;
-    double threshold;
-    double bpm;
-    int counter;
-    int previous;
-    while (_toggled) {
-      values = List.from(_data); // create a copy of the current data array
-      avg = 0;
-      n = values.length;
-      m = 0;
-      for (var value in values) {
-        avg += value.value / n;
-        if (value.value > m) m = value.value;
-      }
-      threshold = (m + avg) / 2;
-      bpm = 0;
-      counter = 0;
-      previous = 0;
-      for (int i = 1; i < n; i++) {
-        if (values[i - 1].value < threshold && values[i].value > threshold) {
-          if (previous != 0) {
-            counter++;
-            bpm +=
-                60 * 1000 / (values[i].time.millisecondsSinceEpoch - previous);
-          }
-          previous = values[i].time.millisecondsSinceEpoch;
-        }
-      }
-      if (counter > 0) {
-        bpm = bpm / counter;
-        if (kDebugMode) {
-          print(bpm);
-        }
-        setState(() {
-          _bpm = ((1 - _alpha) * _bpm + _alpha * bpm).toInt();
-        });
-      }
-      await Future.delayed(Duration(
-          milliseconds:
-              1000 * _windowLen ~/ _fs)); // wait for a new set of _data values
-    }
-  }
+  // void _scanImage(CameraImage image) {
+  //   _now = DateTime.now();
+  //   _avg =
+  //       image.planes.first.bytes.reduce((value, element) => value + element) /
+  //           image.planes.first.bytes.length;
+  //   if (_data.length >= _windowLen) {
+  //     _data.removeAt(0);
+  //   }
+  //   setState(() {
+  //     _data.add(SensorValue(_now ?? DateTime.now(), 255 - (_avg ?? 0.0)));
+  //   });
+  // }
 }
